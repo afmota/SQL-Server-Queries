fcce12162900353c44e7a243142a8934ee86be25
AGNALDO FRANCISCO DA MOTA <af_mota@hotmail.com>
2023-07-03 10:29:48 -0300

    Novas queries
    Inclusão das queries SpidToTextData, Trace, DailyTrace, Deadlocks e Locks

--- Conteção_Locks.sql
+++ Conteção_Locks.sql
@@ -5,4 +5,6 @@ SELECT *
 FROM mirror.DATABASES.LockTablesContencao
 WHERE collection_date > @data
 	AND db_name = 'SaudeSJRP'
+	--AND host_name = 'EMPRO17025'
+	AND program_name LIKE 'UBS%'
 ORDER BY 1
---
+++ Trace.sql
@@ -0,0 +1,30 @@
+USE mirror
+
+DECLARE @data_filtro AS DATETIME
+SET @data_filtro = '2023-06-30 07:00:00.000'
+
+SELECT SPID
+	, TextData
+	, RIGHT('00' + CAST(((Duration / 1000000) / 60) / 60 AS VARCHAR(2)),2) + ':'
+		+ RIGHT('00' + CAST(((Duration / 1000000) / 60) % 60 AS VARCHAR(2)),2) + ':'
+		+ RIGHT('00' + CAST((Duration / 1000000) % 60 AS VARCHAR(2)),2) AS Duration
+	, StartTime
+	, EndTime
+	, ApplicationName
+	, CPU
+	, Reads
+	, Writes
+FROM mirror.DATABASES.DailyTrace_RT
+WHERE StartTime > @data_filtro
+	--AND LoginName <> 'REDE-EMPRO\sqlserveradm'
+	--AND LoginName <> 'etl_dw'
+	--AND ApplicationName NOT LIKE 'SQLAgent%'
+	--AND HostName LIKE 'EMPRO-%'
+	--AND SPID = 587
+	AND ApplicationName LIKE 'UBSE%'
+	--AND TextData LIKE '%USUARIO_SAUDE int,%'
+--ORDER BY Duration DESC
+--ORDER BY CPU DESC
+--ORDER BY Duration DESC
+--ORDER BY StartTime
+--ORDER BY EndTime DESC
