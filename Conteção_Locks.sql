9684d44aa70c272a70dd0620dec65f93821415b7
AGNALDO FRANCISCO DA MOTA <af_mota@hotmail.com>
2023-07-25 17:09:00 -0300

    Update Conteção_Locks.sql

--- Conteção_Locks.sql
+++ Conteção_Locks.sql
@@ -6,7 +6,7 @@ FROM mirror.DATABASES.LockTablesContencao
 WHERE collection_date > @data
 	AND db_name = 'SaudeSJRP'
 	--AND host_name = 'EMPRO17025'
-	--AND program_name LIKE 'UBSF%'
+	AND program_name LIKE 'UBSF3'
 	--AND (wait_info LIKE '%U' OR wait_info LIKE '%X')
 	--AND collection_date > '2023-07-04 07:00'
 ORDER BY 1
